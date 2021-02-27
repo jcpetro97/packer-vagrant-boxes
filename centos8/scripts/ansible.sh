58981e8537a045f420b8883a7523147a79108073
John Petro <john@johnpetro.com>
2021-02-26 22:22:32 -0500

    Made a few modifications, so that certain packages are not uninstalled, so that guest additions can be built should the VB version change.

--- centos8/scripts/ansible.sh
+++ centos8/scripts/ansible.sh
@@ -3,6 +3,6 @@
 # Install Python.
 yum -y install python3 python3-pip
 alternatives --set python /usr/bin/python3
-
+python -m pip install -U pip
 # Install Ansible.
-pip3 install ansible
+python -m pip install ansible==2.9.17
