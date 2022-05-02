bd06bcdf9f7ad30252bc7d4d3e50928b0c98ef68
John Petro <john@johnpetro.com>
2022-05-02 13:40:54 -0400

    trying to fix an error

--- ubuntu2004/scripts/ansible.sh
+++ ubuntu2004/scripts/ansible.sh
@@ -2,10 +2,10 @@
 
 # Install Ansible repository.
 apt -y update && apt-get -y upgrade
-apt -y install software-properties-common
+apt -y install software-properties-common perl make gcc git
 apt-add-repository ppa:ansible/ansible
 
 # Install Ansible.
 apt -y update
 apt -y install ansible
-apt -y install git
+#apt -y install git
